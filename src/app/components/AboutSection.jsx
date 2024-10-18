6acbbdc998d382b30c86b266f14f13611d5f5324
Navodi20 <navodidhananjana2002@gmail.com>
2024-10-18 11:38:48 +0530

    initial commit

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -42,7 +42,7 @@ const TAB_DATA = [
 
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
-  const [isPending, startTransition] = useTransition();
+  const [startTransition] = useTransition(); // Correct usage
 
   const handleTabChange = (id) => {
     startTransition(() => {
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,20 +15,20 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-3xl sm:text-4xl lg:text-6xl lg:leading-tight font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Navodi...",
                 1000,
-                "Web Developer",
+                "an Undergraduate.",
                 1000,
-                "Mobile Developer",
+                "a freelancer.",
                 1000,
-                "UI/UX Designer",
+                "", 
                 1000,
               ]}
               wrapper="span"
@@ -37,8 +37,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+          I am a quick learner and I am always looking to expand my knowledge and skill set. I thrive in collaborative environments where I can contribute my skills while also learning from others. My passion for technology drives me to stay updated with the latest industry trends and best practices, ensuring that I bring fresh ideas and innovative solutions to every project. I enjoy tackling complex challenges and finding effective ways to solve problems.
           </p>
           <div>
             <Link
@@ -48,11 +47,11 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="/#projects"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+                My projects
               </span>
             </Link>
           </div>
@@ -63,14 +62,18 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="relative rounded-full bg-gradient-to-r from-[#181818] via-[#282828] to-[#181818] p-1 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]">
+          <div className="rounded-full bg-[#181818] w-full h-full relative overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
             <Image
-              src="/images/hero-image.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              src="/images/image.png"
+              alt="image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 scale-100 hover:scale-110 transition-transform duration-300"
               width={300}
               height={300}
             />
+          </div>
+
+
           </div>
         </motion.div>
       </div>
