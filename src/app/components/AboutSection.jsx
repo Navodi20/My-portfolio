3f8400d7e67c60e25a3ef7dc2829708770ec4ecd
Navodi20 <navodidhananjana2002@gmail.com>
2024-10-20 18:53:08 +0530

    image correction

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -11,9 +11,9 @@ const TAB_DATA = [
       <ul className="list-disc pl-2">
         <li>Currently reading for a BSc in IT at University of Sri Jayawardenepura</li>
         <li>
-          Advanced Levels at Devi Balika Vidyalaya, Colombo 8 (Art stream) - Achieved 3 A's with a GPA of 1.81
+          Advanced Levels at Devi Balika Vidyalaya,Colombo 8 (Art stream) - Achieved 3 A's with a GPA of 1.81
         </li>
-        <li>Ordinary Levels at Gothami Balika Vidyalaya, Colombo 10 - Achieved 8 A's and 1 B</li>
+        <li>Ordinary Levels at Gothami Balika Vidyalaya,Colombo 10 - Achieved 8 A's and 1 B</li>
       </ul>
     ),
   },
@@ -53,7 +53,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/image 2.jpg" width={500} height={500} alt="About me" />
+        <Image src="./images/image 2.jpg" width={500} height={500} alt="About me" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
